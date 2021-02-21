da3e78b226020a995c745b7ef25579fb047f7130
Connor Todd <cgtodd85@gmail.com>
2021-02-20 23:27:32 -0500

    adding variables thatcreate an array of each string of characters and using console log to test selecting a random index of those arrays

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var upperAlphaVar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerAlphaVar = "abcdefghijklmnopqrstuvwxyz";
-var numbersVar = "0123456789"
-var specialCharVar = "!()?[]`~;:!@#$%^&*+=";
+var upperAlphaString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerAlphaString = "abcdefghijklmnopqrstuvwxyz";
+var numbersString = "0123456789"
+var specialCharString = "!()?[]`~;:!@#$%^&*+=";
+
+var upperAlphaArray = Array.from(upperAlphaString)
+var lowerAlphaArray = Array.from(lowerAlphaString)
+
+
+console.log(upperAlphaArray)
+console.log(upperAlphaArray[Math.floor(Math.random() * upperAlphaArray.length)])
+console.log(upperAlphaArray[Math.floor(Math.random() * upperAlphaArray.length)])
+console.log(upperAlphaArray[Math.floor(Math.random() * upperAlphaArray.length)])
+console.log(upperAlphaArray[Math.floor(Math.random() * upperAlphaArray.length)])
 
 
 ////////////////////Functions////////////////////////
@@ -18,6 +28,7 @@ function writePassword() {
 
 }
 
+
 //////////////////////////////////////////////////
 
 
@@ -29,6 +40,10 @@ generateBtn.addEventListener("click", writePassword);
 //Then prompts for criteria, user selects
 //Then prompts for length, choice between 8 and 128
 //Then prompts for character types
+//If yes for upper case, 
+
+//for loop for length of the password they chose
+//each iteration of the loop 
 
 
 
