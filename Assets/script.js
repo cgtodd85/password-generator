23c39117d410d6129ad378cdcee46b05cb99a529
Connor Todd <cgtodd85@gmail.com>
2021-02-20 22:23:40 -0500

    added varables for character selection and added notes

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperAlphaVar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerAlphaVar = "abcdefghijklmnopqrstuvwxyz";
+var numbersVar = "0123456789"
+var specialCharVar = "!()?[]`~;:!@#$%^&*+=";
+
+
+////////////////////Functions////////////////////////
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +18,23 @@ function writePassword() {
 
 }
 
+//////////////////////////////////////////////////
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//button
+//Then prompts for criteria, user selects
+//Then prompts for length, choice between 8 and 128
+//Then prompts for character types
+
+
+
+// preventDefault() somewhere to prevent page from refreshing
+// clear the "your secure password" placeholder
+// random character generator
+// random num generator
+// random lower
+// random upper
